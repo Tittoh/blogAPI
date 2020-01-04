51bc411303ca9a021fa1784d707bec29bbac40d4
Tittoh <tittohb@gmail.com>
2020-01-04 18:36:32 +0300

    Deploy

--- authors/settings.py
+++ authors/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CORS_ORIGIN_WHITELIST = (
     "http://localhost:8080",
