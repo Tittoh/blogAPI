dce1251fc45435851add341f2dbe9fa32923a790
Tittoh <tittohb@gmail.com>
2020-03-02 12:42:06 +0300

    Add followers and following count to profile

--- authors/apps/profiles/serializers.py
+++ authors/apps/profiles/serializers.py
@@ -8,10 +8,12 @@ class ProfileSerializer(serializers.ModelSerializer):
     email = serializers.CharField(source='user.email')
     bio = serializers.CharField(allow_blank=True, required=False)
     image = serializers.CharField(allow_blank=True, required=False)
+    follows = serializers.SerializerMethodField(method_name="follows_count")
+    followers = serializers.SerializerMethodField(method_name="followers_count")
 
     class Meta:
         model = Profile
-        fields = ('username', 'bio', 'image', 'email',)
+        fields = ('username', 'bio', 'image', 'email', 'follows', 'followers')
         read_only_fields = ('username',)
 
     def get_image(self, obj):
@@ -19,3 +21,9 @@ class ProfileSerializer(serializers.ModelSerializer):
             return obj.image
 
         return 'https://static.productionready.io/images/smiley-cyrus.jpg'
+
+    def follows_count(self, obj):
+        return obj.follows.count()
+
+    def followers_count(self, obj):
+        return obj.get_followers(obj).count()
